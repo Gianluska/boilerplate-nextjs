77a9e3e2b755b5f6060898b8416982b88f351e35
Gianlucca Claudino <gianluccaclaudino@gmail.com>
2020-11-11 15:30:41 -0300

    configurando Storybook, configurando styled component com Jest

--- client/src/components/Main/test.tsx
+++ client/src/components/Main/test.tsx
@@ -12,4 +12,9 @@ describe('<Main />', () => {
 
     expect(container.firstChild).toMatchSnapshot()
   })
+  it('should render the color correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
+  })
 })
